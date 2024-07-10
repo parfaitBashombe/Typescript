4cdb94452b941e5c66f33e6513c7bb7dac588ba3
Parfait Bashombe <parfaitbashombe@gmail.com>
2024-07-10 17:26:40 +0300

    modif

--- Desktop/rock/script.js
+++ Desktop/rock/script.js
@@ -25,4 +25,47 @@ optionImages.forEach((image, index) => {
     // Set a timeout to delay the result calculation
     let time = setTimeout(() => {
       gameContainer.classList.remove("start");
-    }
+
+      // Get the source of the clicked option image
+      let imageSrc = e.target.querySelector("img").src;
+      // Set the user image to the clicked option image
+      userResult.src = imageSrc;
+
+      // Generate a random number between 0 and 2
+      let randomNumber = Math.floor(Math.random() * 3);
+      // Create an array of CPU image options
+      let cpuImages = [
+        "images/rock.png",
+        "images/paper.png",
+        "images/scissors.png",
+      ];
+      // Set the CPU image to a random option from the array
+      cpuResult.src = cpuImages[randomNumber];
+
+      // Assign a letter value to the CPU option (R for rock, P for paper, S for scissors)
+      let cpuValue = ["R", "P", "S"][randomNumber];
+      // Assign a letter value to the clicked option (based on index)
+      let userValue = ["R", "P", "S"][index];
+
+      // Create an object with all possible outcomes
+      let outcomes = {
+        RR: "Draw",
+        RP: "Cpu",
+        RS: "User",
+        PP: "Draw",
+        PR: "User",
+        PS: "Cpu",
+        SS: "Draw",
+        SR: "Cpu",
+        SP: "User",
+      };
+
+      // Look up the outcome value based on user and CPU options
+      let outComeValue = outcomes[userValue + cpuValue];
+
+      // Display the result
+      result.textContent =
+        userValue === cpuValue ? "Match Draw" : `${outComeValue} Won!!`;
+    }, 2500);
+  });
+});
