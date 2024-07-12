8289d542018a9fd5b0eaec5981e80a98cee2cd2b
Parfait Bashombe <parfaitbashombe@gmail.com>
2024-07-12 18:01:25 +0300

    modif

---
+++ ts-hello/point.ts
@@ -0,0 +1,10 @@
+export class Point {
+  constructor(private x?: number, private y?: number) {
+    this.x = x;
+    this.y = y;
+  }
+
+  draw() {
+    console.log("X: " + this.x + ",Y: " + this.y);
+  }
+}
