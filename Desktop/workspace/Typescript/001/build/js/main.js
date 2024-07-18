162d0bb5c73fa6f5ada72ea1bbc5af018de63f26
Parfait Bashombe <parfaitbashombe@gmail.com>
2024-07-18 18:08:21 +0300

    modif

---
+++ Desktop/workspace/Typescript/001/build/js/main.js
@@ -0,0 +1,7 @@
+"use strict";
+let e = "world";
+const addOrConcat = (a, b, c) => {
+    if (c === "add")
+        return a + b;
+    return "" + a + b;
+};
---
+++ Desktop/workspace/Typescript/001/src/main.ts
@@ -0,0 +1,10 @@
+let e = <string | number>"world";
+
+const addOrConcat = (
+  a: number,
+  b: number,
+  c: "add" | "concat"
+): number | string => {
+  if (c === "add") return a + b;
+  return "" + a + b;
+};
