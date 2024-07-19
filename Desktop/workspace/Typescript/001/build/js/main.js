58594e372339e40b34c0470ac2e89c7074385b48
Parfait Bashombe <parfaitbashombe@gmail.com>
2024-07-19 22:00:35 +0300

    modif

--- Desktop/workspace/Typescript/001/build/js/main.js
+++ Desktop/workspace/Typescript/001/build/js/main.js
@@ -1,1 +1,13 @@
 "use strict";
+let e = "world";
+const addOrConcat = (a, b, c) => {
+    if (c === "add")
+        return a + b;
+    return "" + a + b;
+};
+console.log(addOrConcat(1, 2, "add"));
+//variation
+const year = document.getElementById("year");
+const thisYear = new Date().getFullYear().toString();
+year.setAttribute("datetime", thisYear);
+year.textContent = thisYear;
