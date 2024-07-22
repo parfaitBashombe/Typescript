65ef44a1280b138224c4cda2056b61c97297cc88
Parfait Bashombe <parfaitbashombe@gmail.com>
2024-07-22 20:06:54 +0300

    modif

--- Desktop/workspace/Typescript/To_do List/vite-project/src/templates/ListTemplate.ts
+++ Desktop/workspace/Typescript/To_do List/vite-project/src/templates/ListTemplate.ts
@@ -5,3 +5,28 @@ interface DOMList {
   clear(): void;
   render(fullList: FullList): void;
 }
+
+export default class ListTemplate implements DOMList {
+  ul: HTMLUListElement;
+
+  private constructor() {
+    this.ul = document.getElementById("listItems") as HTMLUListElement;
+  }
+
+  clear(): void {
+    this.ul.innerHTML = "";
+  }
+
+  render(fullList: FullList): void {
+    this.clear();
+
+    fullList.list.forEach((item) => {
+      const li = document.createElement("li") as HTMLElement;
+      li.className = "item";
+
+      const check = document.createElement("input") as HTMLInputElement;
+      check.type = "checkbox";
+      check.id = item.id;
+    });
+  }
+}
