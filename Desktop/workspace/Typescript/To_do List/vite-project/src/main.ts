5fae6473c109eefef73e3b9de2f437fab6697023
Parfait Bashombe <parfaitbashombe@gmail.com>
2024-07-22 20:48:35 +0300

    modif

--- Desktop/workspace/Typescript/To_do List/vite-project/src/main.ts
+++ Desktop/workspace/Typescript/To_do List/vite-project/src/main.ts
@@ -1,1 +1,30 @@
 import "../src/css/style.css";
+import FullList from "./model/FullList";
+import ListItem from "./model/ListItem";
+import ListTemplate from "./templates/ListTemplate";
+
+const initApp = (): void => {
+  const fullList = FullList.instance;
+  const template = ListTemplate.instance;
+
+  const itemEntryForm = document.getElementById(
+    "itemEntryForm"
+  ) as HTMLFormElement;
+  itemEntryForm.addEventListener("submit", (event: SubmitEvent): void => {
+    event.preventDefault();
+  });
+
+  const clearItems = document.getElementById(
+    "clearItemButton"
+  ) as HTMLButtonElement;
+
+  clearItems.addEventListener("click", (): void => {
+    fullList.clearList();
+    template.clear();
+  });
+
+  fullList.load();
+  template.render(fullList);
+};
+
+document.addEventListener("DOMContentLoaded", initApp);
--- Desktop/workspace/Typescript/To_do List/vite-project/src/templates/ListTemplate.ts
+++ Desktop/workspace/Typescript/To_do List/vite-project/src/templates/ListTemplate.ts
@@ -9,6 +9,8 @@ interface DOMList {
 export default class ListTemplate implements DOMList {
   ul: HTMLUListElement;
 
+  static instance: ListTemplate = new ListTemplate();
+
   private constructor() {
     this.ul = document.getElementById("listItems") as HTMLUListElement;
   }
@@ -27,6 +29,31 @@ export default class ListTemplate implements DOMList {
       const check = document.createElement("input") as HTMLInputElement;
       check.type = "checkbox";
       check.id = item.id;
+      check.tabIndex = 0;
+      check.checked = item.checked;
+      li.append(check);
+
+      check.addEventListener("change", () => {
+        item.checked = !item.checked;
+        fullList.save();
+      });
+
+      const label = document.createElement("label") as HTMLLabelElement;
+      label.htmlFor = item.id;
+      label.textContent = item.item;
+      li.append(label);
+
+      const button = document.createElement("button") as HTMLButtonElement;
+      button.className = "button";
+      button.textContent = "X";
+      li.append(button);
+
+      button.addEventListener("click", () => {
+        fullList.removeItem(item.id);
+        this.render(fullList);
+      });
+
+      this.ul.append(li);
     });
   }
 }
