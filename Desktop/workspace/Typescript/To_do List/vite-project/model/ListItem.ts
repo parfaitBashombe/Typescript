721a6f71d7ae4159379950ff68b0330d7f4dd4c4
Parfait Bashombe <parfaitbashombe@gmail.com>
2024-07-22 17:46:53 +0300

    modif

---
+++ Desktop/workspace/Typescript/To_do List/vite-project/model/ListItem.ts
@@ -0,0 +1,13 @@
+export interface Item {
+  id: string;
+  item: string;
+  checked: boolean;
+}
+
+export default class ListItem implements Item {
+  constructor(
+    private _id: string = "",
+    private _item: string = "",
+    private _checked: boolean = false
+  ) {}
+}
