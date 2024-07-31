0459ea7d2c8fbd6771af8287b5f7cb17b9ca0ddd
Parfait Bashombe <parfaitbashombe@gmail.com>
2024-07-31 23:28:04 +0300

    modif

--- Desktop/workspace/Typescript/React and Typescript/my-app/src/components/InputField.tsx
+++ Desktop/workspace/Typescript/React and Typescript/my-app/src/components/InputField.tsx
@@ -5,22 +5,3 @@ interface Props {
   todo: string;
   setTodo: React.Dispatch<React.SetStateAction<string>>;
 }
-
-const InputField: React.FC<Props> = ({ todo, setTodo }) => {
-  return (
-    <form className="input">
-      <input
-        type="input"
-        value={todo}
-        onChange={(e) => setTodo(e.target.value)}
-        className="input_box"
-        placeholder="Enter a task"
-      />
-      <button className="input_submit" type="submit">
-        Go
-      </button>
-    </form>
-  );
-};
-
-export default InputField;
