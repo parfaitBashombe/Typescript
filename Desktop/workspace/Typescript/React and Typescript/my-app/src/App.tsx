53fae601865cd624966b8e7d7dcbedf7f4d3b348
Parfait Bashombe <parfaitbashombe@gmail.com>
2024-07-31 23:27:29 +0300

    modif

--- Desktop/workspace/Typescript/React and Typescript/my-app/src/App.tsx
+++ Desktop/workspace/Typescript/React and Typescript/my-app/src/App.tsx
@@ -4,7 +4,7 @@ import InputField from "./components/InputField";
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
-
+  console.log(todo);
   return (
     <div className="App">
       <span className="heading">Taskify</span>
