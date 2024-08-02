0db74881fb8da0f7f61da267f11d0bcbc47ce594
Parfait Bashombe <parfaitbashombe@gmail.com>
2024-08-02 21:46:45 +0300

    modif

--- Desktop/workspace/Typescript/React and Typescript/my-app/src/components/InputField.tsx
+++ Desktop/workspace/Typescript/React and Typescript/my-app/src/components/InputField.tsx
@@ -1,3 +1,11 @@
+import React from "react";
+import "./styles.css";
+
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+
 const InputField: React.FC<Props> = ({ todo, setTodo }) => {
   return (
     <form className="input">
