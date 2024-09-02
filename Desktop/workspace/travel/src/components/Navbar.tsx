02a5039e8b07aa0b3a7ba03931099fda367d5966
Parfait Bashombe <parfaitbashombe@gmail.com>
2024-09-02 17:37:45 +0300

    modif

--- Desktop/workspace/travel/src/components/Navbar.tsx
+++ Desktop/workspace/travel/src/components/Navbar.tsx
@@ -1,7 +1,20 @@
+"use client";
+
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <header className="flex flex-row justify-between p-5 bg-green-900 h-16 items-center">
+      <section id="hero">Travels</section>
+      <nav>
+        <ul className="flex flex-row gap-4">
+          <li>Preview</li>
+          <li>Contact Us</li>
+          <li>About Us</li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
