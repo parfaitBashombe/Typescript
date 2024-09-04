43c21e37972e4ec5fb58bf00773e4d7615b24736
Parfait Bashombe <parfaitbashombe@gmail.com>
2024-09-04 15:41:37 +0300

    modif

--- Desktop/workspace/travel/src/components/footer.tsx
+++ Desktop/workspace/travel/src/components/footer.tsx
@@ -14,13 +14,6 @@ const Footer = () => {
           <br /> an oportunity for adventure, discovery,
           <br /> and unforgettable experiences.
           <br />
-          <p className="my-2">
-            Email:{" "}
-            <Link className="" href="mailto:parfaitbashombe@gmail.com">
-              parfaitbashombe@gmail.com
-            </Link>
-          </p>
-          Phone: <Link href="tel:+2431999999">(+2) 431999999</Link>
         </address>
 
         <nav className="hidden md:flex flex-col gap-2" aria-label="footer">
@@ -39,27 +32,68 @@ const Footer = () => {
         </nav>
 
         <nav className="hidden md:flex flex-col gap-2" aria-label="footer">
-          <Link href="#naviation" className="hover:opacity-90">
-            <h1 className="text-lg">Navigation</h1>
+          <Link href="#tools" className="hover:opacity-90">
+            <h1 className="text-lg">Tools</h1>
           </Link>
-          <Link href="#services" className="hover:opacity-50 opacity-60">
-            Services
+          <Link href="#visualSales" className="hover:opacity-50 opacity-60">
+            Visual sales
           </Link>
-          <Link href="#products" className="hover:opacity-50 opacity-60">
-            Products
+          <Link href="#trendAnalasis" className="hover:opacity-50 opacity-60">
+            Trend Analasis
           </Link>
-          <Link href="#about" className="hover:opacity-50 opacity-60">
-            About
+          <Link
+            href="#customerSegmentation"
+            className="hover:opacity-50 opacity-60"
+          >
+            Customer Segmentation
+          </Link>
+          <Link
+            href="#realTimeDashboard"
+            className="hover:opacity-50 opacity-60"
+          >
+            Real-Time Dashboard
           </Link>
         </nav>
 
-        <div className="flex flex-col sm:gap-2">
-          <p className="text-right">
-            Copyright &copy; <span id="year">{year}</span>
-          </p>
-          <p className="text-right">All Rights Reserved</p>
-        </div>
+        <nav className="hidden md:flex flex-col gap-2" aria-label="footer">
+          <Link href="#address" className="hover:opacity-90">
+            <h1 className="text-lg">Addres</h1>
+          </Link>
+          <Link href="#" className="hover:opacity-50 opacity-60">
+            8589
+          </Link>
+          <Link href="#" className="hover:opacity-50 opacity-60">
+            Preston Rd.Inglewood
+          </Link>
+          <Link href="#" className="hover:opacity-50 opacity-60">
+            Maine
+          </Link>
+          <Link href="#" className="hover:opacity-50 opacity-60">
+            92340
+          </Link>
+        </nav>
+
+        <nav className="hidden md:flex flex-col gap-2" aria-label="footer">
+          <Link href="#contacts" className="hover:opacity-90">
+            <h1 className="text-lg">Contacts</h1>
+          </Link>
+          <Link
+            className="hover:opacity-50 opacity-60"
+            href="mailto:parfaitbashombe@gmail.com"
+          >
+            parfaitbashombe@gmail.com
+          </Link>
+          <Link className="hover:opacity-50 opacity-60" href="tel:+2431999999">
+            (+2) 431999999
+          </Link>
+        </nav>
       </section>
+      <div className="flex flex-col sm:gap-2">
+        <p className="text-left">
+          Copyright &copy; <span id="year">{year}</span>
+        </p>
+        <p className="text-right">All Rights Reserved</p>
+      </div>
     </footer>
   );
 };
