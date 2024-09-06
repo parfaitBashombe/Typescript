f1fd66227c10ddac18646cf5f090b97a6fa2d126
Parfait Bashombe <parfaitbashombe@gmail.com>
2024-09-06 18:31:10 +0300

    modif

--- Desktop/workspace/travel/src/components/promos/promos.tsx
+++ Desktop/workspace/travel/src/components/promos/promos.tsx
@@ -4,7 +4,7 @@ import { MdOutlineArrowOutward } from "react-icons/md";
 
 const Promos = (): JSX.Element => {
   return (
-    <div className="max-w-6xl mx-auto my-10 flex flex-col h-60 bg-green-300">
+    <div className="max-w-6xl mx-auto my-10 flex flex-col">
       <section
         id="bigPromo"
         className="flex flex-col gap-2 md:gap-16 justify-between md:flex-row py-10 text-left"
@@ -26,34 +26,40 @@ const Promos = (): JSX.Element => {
       </section>
       <section
         id="specialPromo"
-        className="h-1/2 flex flex-row justify-evenly gap-10"
+        className="flex flex-row justify-evenly gap-5 bg-green-400 py-10 px-3"
       >
-        <p id="summerPromo" className="flex-1 bg-scroll bg-hero-image bg-cover">
+        <p
+          id="summerPromo"
+          className="flex-1 text-white px-2 bg-scroll bg-hero-image bg-cover h-36 rounded-2xl"
+        >
           Summer Promo
         </p>
         <div
-          id="explorePromo"
-          className="flex flex-row gap-10 h-full bg-red-500"
+          id="explore"
+          className="max-w-96 flex flex-col gap-9 bg-red-500 p-8 rounded-2xl"
         >
-          <div id="explore" className="">
-            <title>{`Let's Explore Together`}</title>
-            <p>
-              At Travel, the world is our playground, and every journey is an
-              oportunity to discover something new. Join us as we explore the
-              wonders of the world and create memories that will last a
-              lifetime.
-            </p>
-          </div>
-          <div id="ramadanPromo" className="">
-            <p>
-              Enjoy a 50% discount on your train tickets. Book now with Travel
-              and embrace the spirit of season as you embark on your
-              unforgettable journey.
-            </p>
-            <title>Ramadan Promo</title>
-          </div>
+          <p>{`Let's Explore Together`}</p>
+          <p className="text-sm">
+            At Travel, the world is our playground, and every journey is an
+            oportunity to discover something new. Join us as we explore the
+            wonders of the world and create memories that will last a lifetime.
+          </p>
         </div>
-        <div id="voyage" className="bg-scroll bg-hero-image bg-cover">
+        <div
+          id="ramadanPromo"
+          className="max-w-80 flex flex-col gap-9 bg-red-500 p-8 rounded-2xl"
+        >
+          <p>
+            Enjoy a 50% discount on your train tickets. Book now with Travel and
+            embrace the spirit of season as you embark on your unforgettable
+            journey.
+          </p>
+          <p>Ramadan Promo</p>
+        </div>
+        <div
+          id="voyage"
+          className="flex-2 bg-scroll bg-hero-image bg-cover flex-1 h-36 rounded-2xl"
+        >
           <button className="p-4 bg-slate-100 rounded-full">
             <MdOutlineArrowOutward />
           </button>
